6a84e430455d8b10b9fbbb74475797229a9821bb
David Goricanec <david.goricanec@edu.fh-joanneum.at>
2023-02-27 22:27:17 +0100

    moved final col names to config

--- convert_dataset.py
+++ convert_dataset.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 import json
+import config
 
 
 trainfile_path = './Base_Dataset/KDDTest_no_headers.csv'
@@ -91,26 +92,9 @@ for chr in text_columns:
     final_df.drop(chr, axis=1, inplace=True)
  #   final_df_test.drop(chr, axis=1, inplace=True)
 
-final_col_names = ["duration", "src_bytes", "dst_bytes", "land", "wrong_fragment",
-             "urgent", "hot", "num_failed_logins", "logged_in", "num_compromised", "root_shell", "su_attempted", "num_root",
-             "num_file_creations", "num_shells", "num_access_files", "num_outbound_cmds", "is_host_login", "is_guest_login", 
-             "count", "srv_count", "serror_rate", "srv_serror_rate", "rerror_rate", "srv_rerror_rate", "same_srv_rate",
-             "diff_srv_rate", "srv_diff_host_rate", "dst_host_count", "dst_host_srv_count", "dst_host_same_srv_rate", 
-             "dst_host_diff_srv_rate", "dst_host_same_src_port_rate", "dst_host_srv_diff_host_rate", "dst_host_serror_rate", 
-             "dst_host_srv_serror_rate", "dst_host_rerror_rate", "dst_host_srv_rerror_rate", "Protocol_icmp","Protocol_tcp",
-             "Protocol_udp","service_IRC","service_X11","service_Z39_50","service_auth","service_bgp","service_courier","service_csnet_ns",
-             "service_ctf","service_daytime","service_discard","service_domain","service_domain_u","service_echo","service_eco_i","service_ecr_i",
-             "service_efs","service_exec","service_finger","service_ftp","service_ftp_data","service_gopher","service_hostnames","service_http",
-             "service_http_443","service_imap4","service_iso_tsap","service_klogin","service_kshell","service_ldap","service_link","service_login",
-             "service_mtp","service_name","service_netbios_dgm","service_netbios_ns","service_netbios_ssn","service_netstat","service_nnsp","service_nntp",
-             "service_ntp_u","service_other","service_pm_dump","service_pop_2","service_pop_3","service_printer","service_private","service_remote_job","service_rje",
-             "service_shell","service_smtp","service_sql_net","service_ssh","service_sunrpc","service_supdup","service_systat","service_telnet","service_tftp_u",
-             "service_tim_i","service_time","service_urp_i","service_uucp","service_uucp_path","service_vmnet","service_whois","flag_OTH","flag_REJ","flag_RSTO",
-             "flag_RSTOS0","flag_RSTR","flag_S0","flag_S1","flag_S2","flag_S3","flag_SF","flag_SH","class_"]
-
-final_df = final_df[final_col_names]
+final_df = final_df[config.final_col_names]
 final_df.to_csv('./Data/train_enc.csv', index=False)
 #final_df_test.to_csv('./Data/test_enc.csv', index=False)
 
-np.savetxt('final_col_names.txt', final_col_names, delimiter=',', fmt='%s')
+np.savetxt('final_col_names.txt', config.final_col_names, delimiter=',', fmt='%s')
 print("Saved!")
